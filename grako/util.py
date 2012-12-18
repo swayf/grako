5fdcba12134607e78b661bc161333925e3051b78
Juancarlo Añez <juancarlo.anez@gmail.com>
2012-12-17 19:38:31 -0430

    A definition of an AST.

--- grako/util.py
+++ grako/util.py
@@ -1,18 +1,52 @@
 import functools
-from collections import OrderedDict
+from collections import OrderedDict, Mapping
 
-class AttributeDict(OrderedDict):
-    def __missing__(self, name):
-        return list()
+class AST(Mapping):
+    def __init__(self):
+        self.elements = OrderedDict()
 
-    def __getattribute__(self, name):
+    def __iter__(self):
+        return iter(self.elements)
+
+    def __contains__(self, value):
+        return value in self.elements
+
+    def __len__(self):
+        return len(self.elements)
+
+    def __getitem__(self, key):
+        if key not in self.elements:
+            self.elements[key] = list()
+        return self.elements[key]
+
+    def __getattr__(self, name):
         if name in self:
             return self[name]
+
+    @staticmethod
+    def pprint(arg, depth=0):
+        indent = ' ' * 4 * depth
+        indent1 = ' ' * 4 * (depth + 1)
+        result = ''
+        if isinstance(arg, list):
+            result += '\n' + indent + '[\n'
+            for e in arg:
+                result += AST.pprint(e, depth + 1) + '\n'
+            result += indent + ']\n'
+            return result
+        elif isinstance(arg, AST):
+            result += '\n' + indent + 'ast{\n'
+            for k in arg.keys():
+                result += indent1 + str(k) + ':'
+                value = arg[k]
+                result += AST.pprint(value, depth + 1) + '\n'
+            result += indent + '}\n'
+            return result
         else:
-            return super(AttributeDict, self).__getattribute__(name)
+            return indent1 + str(arg)
 
     def __repr__(self):
-        return dict.__repr__(self)
+        return self.pprint(self)
 
 
 def memoize(func):
