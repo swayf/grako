f4142a795d1d8568e21c8b4c22bcfb643a5004e3
Juancarlo Añez <juancarlo.anez@gmail.com>
2012-12-19 18:58:00 -0430

    Need to sting.decode('string-escape'), but not sure where.

--- run.py
+++ run.py
@@ -8,10 +8,11 @@ def main():
     text = open('etc/grako.ebnf').read()
     g = GrakoGrammar()
     _result = g.parse('grammar', text)
-#    print g.ast
+    print g.ast
     open('0.ebnf', 'w').write(text)
 
     print '-' * 40, 'phase 1'
+    text = open('0.ebnf').read()
     g = GrakoParserGenerator()
     g.parse('grammar', text)
     generated_grammar = str(g.ast['grammar'][0])
@@ -20,18 +21,21 @@ def main():
 
 
     print '-' * 40, 'phase 2'
+    text = open('1.ebnf').read()
     g = GrakoParserGenerator()
-    g.parse('grammar', generated_grammar)
-    generated_grammar2 = open('1.ebnf', 'r').read()
+    g.parse('grammar', text)
+    generated_grammar2 = str(g.ast['grammar'][0])
     print generated_grammar2
-    open('2.ebnf', 'w').write(generated_grammar2)
+    open('2.ebnf', 'w').write(generated_grammar2.decode('string-escape'))
 
     print '-' * 40, 'phase 3'
+    text = open('2.ebnf').read()
     g = GrakoParserGenerator()
-    g.parse('grammar', generated_grammar2)
-    generated_grammar3 = open('2.ebnf', 'r').read()
+    g.parse('grammar', text)
+    generated_grammar3 = str(g.ast['grammar'][0])
     print generated_grammar3
-    open('3.ebnf', 'w').write(generated_grammar3)
+    assert generated_grammar2 == generated_grammar3
+    open('3.ebnf', 'w').write(generated_grammar3.decode('string-escape'))
 
 if __name__ == '__main__':
     main()
