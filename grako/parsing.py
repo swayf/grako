cc617118ce06a8fd39a90e871a06276ddf82eb67
Juancarlo Añez <juancarlo.anez@gmail.com>
2012-12-26 19:56:13 -0430

    Serious simplification of parsing engine.

--- grako/parsing.py
+++ grako/parsing.py
@@ -14,28 +14,30 @@ def check(result):
 
 class Context(object):
     def __init__(self, rules, text):
-        self.rules = {rule.name :rule.exp for rule in rules}
+        self.rules = {rule.name :rule for rule in rules}
         self.buf = Buffer(text)
         self.buf.goto(0)
+        self._rule_stack = []
 
+    def gotp(self, pos):
+        self.buf.goto(pos)
+
+    @property
+    def pos(self):
+        return self.buf.pos
+
+    def rulestack(self):
+        return '.'.join(self._rule_stack)
 
 class _Parser(object):
 
     def parse(self, ctx):
-        result, newpos = self._parse(ctx, ctx.buf.pos)
-        ctx.buf.goto(newpos)
-        return result
-
-    def _parse(self, ctx, pos):
-        return None, pos
-
+        return None
 
 class EOFParser(_Parser):
-    def _parse(self, ctx, pos):
-        ctx.buf.goto(pos)
+    def parse(self, ctx):
         if not ctx.buf.atend():
             raise FailedParse(ctx.buf, '<EOF>')
-        return None, ctx.buf.pos
 
 
 class _DecoratorParser(_Parser):
@@ -44,8 +46,8 @@ class _DecoratorParser(_Parser):
         super(_DecoratorParser, self).__init__()
         self.exp = exp
 
-    def _parse(self, ctx, pos):
-        return self.exp.parse(ctx), ctx.buf.pos
+    def parse(self, ctx):
+        return self.exp.parse(ctx)
 
     def __str__(self):
         return str(self.exp)
@@ -61,13 +63,12 @@ class TokenParser(_Parser):
         super(TokenParser, self).__init__()
         self.token = token
 
-    def _parse(self, ctx, pos):
-        log.info('token %s\n\t%s', self.token, ctx.buf.lookahead())
-        ctx.buf.goto(pos)
+    def parse(self, ctx):
+        log.info('token <%s>\n\t%s', self.token, ctx.buf.lookahead())
         result = ctx.buf.match(self.token)
         if result is None:
             raise FailedToken(ctx.buf, self.token)
-        return result, ctx.buf.pos
+        return result
 
     def __str__(self):
         return "'%s'" % self.token.encode('string-escape')
@@ -79,14 +80,12 @@ class PatternParser(_Parser):
         self.pattern = pattern
         self.re = re.compile(pattern)
 
-    def _parse(self, ctx, pos):
-        log.info('pattern %s\n\t%s', self.pattern, ctx.buf.lookahead())
-        ctx.buf.goto(pos)
+    def parse(self, ctx):
+        log.info('pattern <%s>\n\t%s', self.pattern, ctx.buf.lookahead())
         result = ctx.buf.matchre(self.re)
         if result is None:
             raise FailedPattern(ctx.buf, self.pattern)
-        print 'newpos', ctx.buf.pos, ctx.buf.lookahead()
-        return result, ctx.buf.pos
+        return result
 
     def __str__(self):
         return '?/%s/?' % self.pattern
@@ -98,12 +97,11 @@ class SequenceParser(_Parser):
         assert isinstance(sequence, list), str(sequence)
         self.sequence = sequence
 
-    def _parse(self, ctx, pos):
-        return self.parse_seq(ctx, pos, self.sequence), ctx.buf.pos
+    def parse(self, ctx):
+        return self.parse_seq(ctx, self.sequence)
 
-    def parse_seq(self, ctx, pos, seq):
+    def parse_seq(self, ctx, seq):
         log.debug('sequence %s', str([type(s) for s in self.sequence]))
-        ctx.buf.goto(pos)
         result = []
         for i, s in enumerate(seq):
             if not isinstance(s, CutParser):
@@ -114,7 +112,7 @@ class SequenceParser(_Parser):
                     result.extend(self.parse_seq(ctx, ctx.buf.pos, seq[i + 1:]))
                 except FailedParse as e:
                     raise FailedCut(ctx.buf, e)
-        return result  # [r for r in result if r is not None]
+        return [r for r in result if r is not None]
 
     def __str__(self):
         return ' '.join(str(s).strip() for s in self.sequence)
@@ -126,12 +124,13 @@ class ChoiceParser(_Parser):
         assert isinstance(options, list), repr(options)
         self.options = options
 
-    def _parse(self, ctx, pos):
+    def parse(self, ctx):
         items = []
+        pos = ctx.pos
         for o in self.options:
-            ctx.buf.goto(pos)
+            ctx.goto(pos)
             try:
-                return o.parse(ctx), ctx.buf.pos
+                return o.parse(ctx)
             except FailedCut as e:
                 raise e.nested
             except FailedParse as e:
@@ -143,9 +142,8 @@ class ChoiceParser(_Parser):
 
 
 class RepeatParser(_DecoratorParser):
-    def _parse(self, ctx, pos):
+    def parse(self, ctx):
         log.info('repeat %s', str(self.exp))
-        ctx.buf.goto(pos)
         result = []
         while True:
             p = ctx.buf.pos
@@ -153,43 +151,41 @@ class RepeatParser(_DecoratorParser):
                 tree = self.exp.parse(ctx)
                 result.append(tree)
             except FailedCut:
-                ctx.buf.goto(p)
                 raise
             except FailedParse:
                 ctx.buf.goto(p)
                 break
-        return result, ctx.buf.pos
+        return result
 
     def __str__(self):
         return '{%s}' % str(self.exp)
 
 
 class RepeatOneParser(RepeatParser):
-    def _parse(self, ctx, pos):
+    def parse(self, ctx):
         head = self.exp.parse(ctx)
-        tail = super(RepeatOneParser, self).parse(ctx)
-        return [head] + tail, ctx.buf.pos
+        return [head] + super(RepeatOneParser, self).parse(ctx)
 
     def __str__(self):
         return '{%s}+' % str(self.exp)
 
 
 class OptionalParser(_DecoratorParser):
-    def _parse(self, ctx, pos):
-        ctx.buf.goto(pos)
+    def parse(self, ctx):
+        p = ctx.pos
         try:
-            return self.exp.parse(ctx), ctx.buf.pos
+            return self.exp.parse(ctx)
         except FailedParse:
-            ctx.buf.goto(pos)
-            return None, pos
+            ctx.goto(p)
+            return None
 
     def __str__(self):
         return '[%s]' % str(self.exp)
 
 
 class CutParser(_Parser):
-    def _parse(self, ctx, pos):
-        return None, pos
+    def parse(self, ctx):
+        return None
 
     def __str__(self):
         return '!'
@@ -200,13 +196,14 @@ class RuleRefParser(_Parser):
         super(RuleRefParser, self).__init__()
         self.name = name
 
-    def _parse(self, ctx, pos):
-        log.info('ref %s %d %s', self.name, pos, ctx.buf.lookahead())
+    def parse(self, ctx):
         try:
             rule = ctx.rules[self.name]
-            return rule.parse(ctx), ctx.buf.pos
+            return rule.parse(ctx)
         except KeyError:
             raise FailedRef(ctx.buf, self.name)
+        except FailedParse:
+            raise
 
     def __str__(self):
         return self.name
@@ -218,9 +215,17 @@ class NamedParser(_DecoratorParser):
         assert isinstance(exp, _Parser), str(exp)
         self.name = name
 
-    def _parse(self, ctx, pos):
-        tree = self.exp.parse(ctx)
-        return Named(name=self.name, value=tree), ctx.buf.pos
+    def parse(self, ctx):
+        ctx._rule_stack.append(self.name)
+        log.info('%s \n\t%s', ctx.rulestack(), ctx.buf.lookahead())
+        try:
+            tree = self.exp.parse(ctx)
+            log.info('SUCCESS %s \n\t%s', ctx.rulestack(), ctx.buf.lookahead())
+            return Named(name=self.name, value=tree)
+        except FailedParse:
+            log.info('FAIL %s \n\t%s', ctx.rulestack(), ctx.buf.lookahead())
+        finally:
+            ctx._rule_stack.pop()
 
     def __str__(self):
         return '%s:%s' % (self.name, str(self.exp))
@@ -235,30 +240,17 @@ class SpecialParser(_Parser):
         return '?/%s/?' % self.pattern
 
 class RuleParser(NamedParser):
-    def _parse(self, ctx, pos):
-        ctx.buf.goto(pos)
-        log.info('enter %s %d %s', self.name, pos, ctx.buf.lookahead())
-        try:
-            tree, pos = self.exp.parse(ctx)
-            log.info('exit %s', self.name)
-        except FailedPattern:
-            log.info('failed %s', self.name)
-            raise FailedParse(ctx.buf, self.name)
-        except FailedParse as e:
-            log.info('failed %s', self.name)
-            raise FailedMatch(ctx.buf, self.name, e.item)
-        except:
-            log.info('failed %s', self.name)
-            raise
+    def parse(self, ctx):
+        tree = self.exp.parse(ctx)
 
         if not isinstance(tree, list):
-            return tree, ctx.buf.pos
+            return tree
         else:
             result = AST()
             for d in tree:
                 if isinstance(d, Named):
                     result[d.name] = d.value
-            return result, ctx.buf.pos
+            return result
 
     def __str__(self):
         return '%s = %s ;' % (self.name, str(self.exp).strip())
