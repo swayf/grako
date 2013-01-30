3617e8e8128476c0a7c1d5e4dc8ba6e7df8527b4
Juancarlo Añez <juancarlo.anez@gmail.com>
2013-01-30 08:37:09 -0430

    Preparation for PyPi publication.

---
+++ setup.py
@@ -0,0 +1,13 @@
+setup(
+    name='Grako',
+    version='1.0rc1',
+    author='Juancarlo Añez',
+    author_email='apalala@gmail.com',
+    packages=['grako', 'grako.model', 'grako.test'],
+    scripts=[],
+    url='http://bitbucket.org/apalala/grako',
+    license='LICENSE.txt',
+    description='EBNF to PEG parser generator.',
+    long_description=open('README.rst').read(),
+    install_requires=[]
+)
