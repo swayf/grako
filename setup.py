f08bae46a03d96ffd2c427eff280906b97b2a43b
Juancarlo Añez <juancarlo.anez@gmail.com>
2013-01-30 09:32:21 -0430

    Added Trove classfiers.

--- setup.py
+++ setup.py
@@ -11,6 +11,21 @@ setup(
     url='http://bitbucket.org/apalala/grako',
     license='LICENSE.txt',
     description='EBNF to PEG parser generator.',
-    long_description=open('README.rst').read(),
-    install_requires=[]
+    long_description=open('README.txt').read(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console', 
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Compilers',
+        'Topic :: Software Development :: Interpreters',
+        'Topic :: Text Processing :: General'
+        ],
 )
