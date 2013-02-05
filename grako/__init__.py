08d4dc83ad048a1142158fc62532051fb71acb17
Juancarlo Añez <juancarlo.anez@gmail.com>
2013-02-05 17:36:10 -0430

    Renamed parse and generate to genmodel and gencode.

--- grako/__init__.py
+++ grako/__init__.py
@@ -22,7 +22,7 @@ argparser = argparse.ArgumentParser(prog='grako',
 
 argparser.add_argument('filename',
                        metavar='grammar',
-                       help='The filename of the grammar to generate a parser for'
+                       help='The filename of the grammar to gencode( a parser for'
                        )
 argparser.add_argument('-m', '--name',
                        nargs=1,
@@ -38,12 +38,12 @@ argparser.add_argument('-t', '--trace',
                        action='store_true'
                        )
 
-def parse(name, grammar, trace=False, filename=None):
+def genmodel(name, grammar, trace=False, filename=None):
     parser = GrakoGrammarGenerator(name, trace=trace)
     return parser.parse(grammar, filename=filename)
 
-def generate(name, grammar, trace=False, filename=None):
-    model = parse(name, grammar, trace=trace, filename=filename)
+def gencode(name, grammar, trace=False, filename=None):
+    model = genmodel(name, grammar, trace=trace, filename=filename)
     return model.render()
 
 
@@ -62,7 +62,7 @@ def main():
         os.unlink(outfile)
     grammar = open(filename, 'r').read()
     try:
-        parser = generate(filename, name, grammar, trace=args.trace)
+        parser = gencode(filename, name, grammar, trace=args.trace)
         if outfile:
             dirname = os.path.dirname(outfile)
             if dirname and not os.path.isdir(dirname):
