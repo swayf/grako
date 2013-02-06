e80a58cc93e5f5d6cd7a0f2264ee9aa9e779f658
Juancarlo Añez <juancarlo.anez@gmail.com>
2013-02-05 21:07:09 -0430

    functools no longer used.

--- grako/util.py
+++ grako/util.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
-import functools
 
 __all__ = ['simplify', 'memoize', 'trim', 'indent']
 
