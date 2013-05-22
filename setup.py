aee87ac2af2003b2bc5a6b3d32f26ab77e7f14e4
oleg <obutovich@gmail.com>
2013-05-22 22:22:56 +0200

    remove python 3.1 from support

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Code Generators',
