443e6cb0a8238fa62decf5a5ecbf2ef7fa249367
oleg <obutovich@gmail.com>
2013-05-22 22:40:32 +0200

    oops.. python 2.6 also taken from config

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         'Intended Audience :: Science/Research',
         'Environment :: Console',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
